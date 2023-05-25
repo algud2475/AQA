3959e94a6ae5650557cbed5f18de85661450b0fa
algud2475 <Algud2475@yandex.ru>
2023-05-25 15:28:40 +0500

    Version 19.05.2023_6:00 (GMT+3)

--- src/test/java/steps/UserSteps.java
+++ src/test/java/steps/UserSteps.java
@@ -3,8 +3,19 @@ package steps;
 import models.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public class UserSteps {
+    public static User getUserById(List<User> users, Integer id) {
+        Optional<User> userOpt = users.stream().filter(user -> user.getId().equals(id)).findFirst();
+        if(userOpt.isPresent()) {
+            return userOpt.get();
+        } else {
+            return null;
+        }
+    }
+
+    /*
     public static User getUserById(List<User> users, Integer id) {
         for (User us : users) {
             if (us.getId() == id) {
@@ -13,4 +24,6 @@ public class UserSteps {
         }
         return null;
     }
+
+     */
 }
